bbab057a8503ec9629ea3fa97a2a9a2be878b086
MartinBgt <martin.bagot@hotmail.com>
2025-02-19 09:18:27 +0100

    navbar responsive v1

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -1,37 +1,55 @@
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer className="bg-p-gray text-p-black py-6 px-28">
-            <div className="grid grid-cols-2 grid-rows-2 gap-4 relative">
-                {/* Première div à gauche */}
-                <div className="col-span-1 row-span-2 flex flex-col space-y-1">
-                    <Link to="/" className="w-max hover:text-pink-dark">Accueil</Link>
-                    <Link to="/chevaigne" className="w-max hover:text-pink-dark">Club de Chevaigné</Link>
-                    <Link to="/saint-gregoire" className="w-max hover:text-pink-dark">Club de Saint-Grégoire</Link>
-                    <Link to="/saulnieres" className="w-max hover:text-pink-dark">Club de Saulnières</Link>
-                    <Link to="/enseignants" className="w-max hover:text-pink-dark">Enseignants</Link>
-                    <Link to="/actualités" className="w-max hover:text-pink-dark">Actualités</Link>
-                    <Link to="/compétitions" className="w-max hover:text-pink-dark">Compétitions</Link>
-                    <Link to="/boutique" className="w-max hover:text-pink-dark">Boutique</Link>
-                    <Link to="/nous-rejoindre" className="w-max hover:text-pink-dark">Nous rejoindre</Link>
+        <footer className="bg-p-white text-p-black py-4 px-28 border-t-p-black border-[1px]">
+            <div className="grid grid-cols-3 items-center justify-evenly">
+                {/* Logo ou espace réservé */}
+                <div className="flex items-center justify-center">
+                    <NavLink to="/" className="h-full flex justify-center items-center">
+                        <img className="h-36 w-auto" src="/Images/AHB/logo_sans_fon_noir.png" alt="Logo de L'AHB"/>
+                    </NavLink>
                 </div>
 
-                {/* Deuxième div en haut à droite */}
-                <div className="col-start-2 row-start-1 flex flex-col items-end h-full space-y-1">
-                    <a href="mailto:contact@parade-riposte.net"
-                       className="hover:text-pink-dark">contact@parade-riposte.net</a>
-                    <a href="https://www.facebook.com/Escrime.AHB/?locale=fr_FR"
-                       target="_blank" className="hover:text-pink-dark">Facebook</a>
-                    <a href="https://www.instagram.com/ahb_escrime/?hl=fr"
-                       target="_blank" className="hover:text-pink-dark">Instagram</a>
+                {/* Navigation principale */}
+                <div className="flex justify-evenly">
+                    <div className="flex flex-col w-full">
+                        <h3 className="font-bold text-2xl pb-4">Navigation</h3>
+                        <Link to="/" className="w-max hover:text-pink-dark">Accueil</Link>
+                        <Link to="/chevaigne" className="w-max hover:text-pink-dark">Club de Chevaigné</Link>
+                        <Link to="/saint-gregoire" className="w-max hover:text-pink-dark">Club de Saint-Grégoire</Link>
+                        <Link to="/saulnieres" className="w-max hover:text-pink-dark">Club de Saulnières</Link>
+                        <Link to="/enseignants" className="w-max hover:text-pink-dark">Enseignants</Link>
+                        <Link to="/actualités" className="w-max hover:text-pink-dark">Actualités</Link>
+                        <Link to="/compétitions" className="w-max hover:text-pink-dark">Compétitions</Link>
+                        <Link to="/boutique" className="w-max hover:text-pink-dark">Boutique</Link>
+                        <Link to="/nous-rejoindre" className="w-max hover:text-pink-dark">Nous rejoindre</Link>
+                    </div>
+                    {/* Informations de contact et réseaux sociaux */}
+                    <div className="flex flex-col w-full">
+                        <h3 className="font-bold text-2xl pb-4">Contact</h3>
+                        <a href="mailto:contact@parade-riposte.net" className="hover:text-pink-dark">
+                            contact@parade-riposte.net
+                        </a>
+                        <a href="https://www.facebook.com/Escrime.AHB/?locale=fr_FR"
+                           target="_blank" rel="noopener noreferrer"
+                           className="hover:text-pink-dark">
+                            Facebook
+                        </a>
+                        <a href="https://www.instagram.com/ahb_escrime/?hl=fr"
+                           target="_blank" rel="noopener noreferrer"
+                           className="hover:text-pink-dark">
+                            Instagram
+                        </a>
+                    </div>
                 </div>
+            </div>
 
-
-                {/* Span en bas à droite */}
-                <span className="absolute bottom-0 right-0">
-                    &copy; {new Date().getFullYear()} | Crée par Martin Bagot.
-                </span>
+            {/* Copyright */}
+            <div className="flex justify-center items-center">
+                <span className="text-center mt-4 text-sm text-gray-600">
+                    &copy; {new Date().getFullYear()} | Créé par Martin Bagot.
+            </span>
             </div>
         </footer>
     );
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,13 +1,14 @@
 import {useEffect, useRef, useState} from "react";
 import {NavLink, useLocation} from "react-router-dom";
+import {Menu, X} from "lucide-react"; // Icônes pour le menu
 import Cta from "../ui/Cta.jsx";
 
 const Navbar = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
-    const dropdownRef = useRef(null); // Référence à l'élément du dropdown
-    const location = useLocation(); // Récupérer l'URL actuelle
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const dropdownRef = useRef(null);
+    const location = useLocation();
 
-    // Vérifie si l'URL actuelle est celle d'un club
     const isClubActive = ["/chevaigne", "/saulnieres", "/saint-gregoire"].includes(location.pathname);
 
     const SpanLinksStyle = ({isActive}) =>
@@ -15,14 +16,12 @@ const Navbar = () => {
             ? "relative text-p-black leading-8 no-underline before:content-[''] before:absolute before:bottom-[-0.25rem] before:left-0 before:w-full before:h-[2px] before:bg-pink-light before:rounded-sm"
             : "relative text-p-black leading-8 no-underline before:content-[''] before:absolute before:bottom-[-0.25rem] before:right-0 before:w-0 before:h-[2px] before:bg-pink-light before:rounded-sm before:transition-all before:duration-400 hover:before:w-full hover:before:left-0";
 
-    // Fermer le dropdown si un clic en dehors est détecté
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
                 setDropdownOpen(false);
             }
         };
-
         document.addEventListener("mousedown", handleClickOutside);
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
@@ -30,75 +29,131 @@ const Navbar = () => {
     }, []);
 
     return (
-        <nav className="bg-white shadow-md p-4 flex justify-between items-center h-20 px-28">
-            <div className="flex items-center space-x-10">
+        <>
+            <nav
+                className="fixed top-0 left-0 w-full bg-white shadow-md p-4 flex justify-between items-center h-20 px-6 md:px-28 z-50">
                 {/* Logo */}
-                <NavLink to="/" className="h-full flex items-center mr-8">
-                    <img className="h-16 w-auto" src="/Images/AHB/logo_sans_fon_noir.png" alt="Logo de L'AHB"/>
+                <NavLink to="/" className="flex items-center">
+                    <img className="h-12 md:h-16 w-auto" src="/Images/AHB/logo_sans_fon_noir.png" alt="Logo de L'AHB"/>
                 </NavLink>
 
-                {/* Liens de navigation */}
-                <NavLink
-                    to="/"
-                    className={({isActive}) => `h-full flex items-center ${SpanLinksStyle({isActive})}`}
-                >
-                    Accueil
-                </NavLink>
+                {/* Menu Desktop */}
+                <div className="hidden md:flex items-center space-x-10">
+                    <NavLink to="/"
+                             className={({isActive}) => `h-full flex items-center ${SpanLinksStyle({isActive})}`}>
+                        Accueil
+                    </NavLink>
 
-                {/* Dropdown "Nos Clubs" */}
-                <div
-                    className="relative h-full flex items-center cursor-pointer"
-                    onClick={() => setDropdownOpen(!dropdownOpen)}
-                    ref={dropdownRef}
-                >
-                    <span className={`h-full flex items-center ${SpanLinksStyle({isActive: isClubActive})}`}>
-                        Nos clubs
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7 10L12 15L17 10" stroke="#000000" strokeWidth="1.5" strokeLinecap="round"
-                                  strokeLinejoin="round"/>
-                        </svg>
-                    </span>
+                    {/* Dropdown "Nos Clubs" */}
+                    <div
+                        className="relative h-full flex items-center cursor-pointer"
+                        onClick={() => setDropdownOpen(!dropdownOpen)}
+                        ref={dropdownRef}
+                    >
+                        <span className={`h-full flex items-center ${SpanLinksStyle({isActive: isClubActive})}`}>
+                            Nos clubs
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7 10L12 15L17 10" stroke="#000000" strokeWidth="1.5" strokeLinecap="round"
+                                      strokeLinejoin="round"/>
+                            </svg>
+                        </span>
 
-                    {/* Menu déroulant */}
-                    {dropdownOpen && (
-                        <div className="absolute top-full left-0 bg-white shadow-lg mt-2 w-40 z-10">
-                            <NavLink to="/chevaigne" className="block px-4 py-2 text-p-black hover:bg-p-gray">
-                                Chevaigné
-                            </NavLink>
-                            <NavLink to="/saint-gregoire" className="block px-4 py-2 text-p-black hover:bg-p-gray">
-                                Saint-Grégoire
-                            </NavLink>
-                            <NavLink to="/saulnieres" className="block px-4 py-2 text-p-black hover:bg-p-gray">
-                                Saulnières
-                            </NavLink>
-                        </div>
-                    )}
+                        {dropdownOpen && (
+                            <div className="absolute top-full left-0 bg-white shadow-lg mt-2 w-40 z-10">
+                                <NavLink to="/chevaigne" className="block px-4 py-2 text-p-black hover:bg-p-gray">
+                                    Chevaigné
+                                </NavLink>
+                                <NavLink to="/saint-gregoire" className="block px-4 py-2 text-p-black hover:bg-p-gray">
+                                    Saint-Grégoire
+                                </NavLink>
+                                <NavLink to="/saulnieres" className="block px-4 py-2 text-p-black hover:bg-p-gray">
+                                    Saulnières
+                                </NavLink>
+                            </div>
+                        )}
+                    </div>
+
+                    <NavLink to="/actualités"
+                             className={({isActive}) => `h-full flex items-center ${SpanLinksStyle({isActive})}`}>
+                        Actualités
+                    </NavLink>
+                    <NavLink to="/compétitions"
+                             className={({isActive}) => `h-full flex items-center ${SpanLinksStyle({isActive})}`}>
+                        Compétitions
+                    </NavLink>
+                    <NavLink to="/boutique"
+                             className={({isActive}) => `h-full flex items-center ${SpanLinksStyle({isActive})}`}>
+                        Boutique
+                    </NavLink>
                 </div>
-                <NavLink
-                    to="/actualités"
-                    className={({isActive}) => `h-full flex items-center ${SpanLinksStyle({isActive})}`}
-                >
-                    Actualités
-                </NavLink>
-                <NavLink
-                    to="/compétitions"
-                    className={({isActive}) => `h-full flex items-center ${SpanLinksStyle({isActive})}`}
-                >
-                    Compétitions
-                </NavLink>
-                <NavLink
-                    to="/boutique"
-                    className={({isActive}) => `h-full flex items-center ${SpanLinksStyle({isActive})}`}
-                >
-                    Boutique
-                </NavLink>
-            </div>
 
-            {/* Bouton CTA */}
-            <NavLink to="/nous-rejoindre" className="h-full flex items-center">
-                <Cta>Nous rejoindre</Cta>
-            </NavLink>
-        </nav>
+                {/* CTA & Menu Burger */}
+                <div className="flex items-center gap-4">
+                    <NavLink to="/nous-rejoindre" className="hidden md:flex">
+                        <Cta>Nous rejoindre</Cta>
+                    </NavLink>
+
+                    {/* Menu Burger */}
+                    <button className="md:hidden" onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+                        {mobileMenuOpen ? <X size={32}/> : <Menu size={32}/>}
+                    </button>
+                </div>
+            </nav>
+
+            {/* Menu Mobile */}
+            {mobileMenuOpen && (
+                <div className="fixed top-20 left-0 w-full bg-white shadow-lg p-6 flex flex-col space-y-4 z-50">
+                    <NavLink to="/" className="text-lg" onClick={() => setMobileMenuOpen(false)}>
+                        Accueil
+                    </NavLink>
+
+                    {/* Dropdown Mobile */}
+                    <div>
+                        <button className="text-lg flex justify-between w-full"
+                                onClick={() => setDropdownOpen(!dropdownOpen)}>
+                            Nos clubs
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7 10L12 15L17 10" stroke="#000000" strokeWidth="1.5" strokeLinecap="round"
+                                      strokeLinejoin="round"/>
+                            </svg>
+                        </button>
+                        {dropdownOpen && (
+                            <div className="pl-4 mt-2 space-y-2">
+                                <NavLink to="/chevaigne" className="block" onClick={() => setMobileMenuOpen(false)}>
+                                    Chevaigné
+                                </NavLink>
+                                <NavLink to="/saint-gregoire" className="block"
+                                         onClick={() => setMobileMenuOpen(false)}>
+                                    Saint-Grégoire
+                                </NavLink>
+                                <NavLink to="/saulnieres" className="block" onClick={() => setMobileMenuOpen(false)}>
+                                    Saulnières
+                                </NavLink>
+                            </div>
+                        )}
+                    </div>
+
+                    <NavLink to="/actualités" className="text-lg" onClick={() => setMobileMenuOpen(false)}>
+                        Actualités
+                    </NavLink>
+                    <NavLink to="/compétitions" className="text-lg" onClick={() => setMobileMenuOpen(false)}>
+                        Compétitions
+                    </NavLink>
+                    <NavLink to="/boutique" className="text-lg" onClick={() => setMobileMenuOpen(false)}>
+                        Boutique
+                    </NavLink>
+
+                    <NavLink to="/nous-rejoindre">
+                        <Cta>Nous rejoindre</Cta>
+                    </NavLink>
+                </div>
+            )}
+
+            {/* Pour éviter que le contenu soit caché derrière la navbar */}
+            <div className="h-20"></div>
+        </>
     );
 };
 
